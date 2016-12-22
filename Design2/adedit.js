d6fe182888e1bc934334f5fe073414625645f569
ghooost <ghooost@gmail.com>
2016-12-22 10:20:28 +0300

    Global update
    Images, data structure, layers rendering

--- Design2/adedit.js
+++ Design2/adedit.js
@@ -6,10 +6,32 @@ $.widget("snark.adedit",{
 		this.page1.init(this);
 		this.page2.init(this);
 		this.page3.init(this);
-		
+
+
+		bannersData.images=[];
+		for(var o in bannersData.templates){
+			var tData=bannersData.templates[o];
+			this.imageCollectionAdd(tData.sys.bg);
+			this.imageCollectionAdd(tData.sys.logo);
+			this.imageCollectionAdd(tData.custom.bg);
+			this.imageCollectionAdd(tData.custom.logo);
+		}
+
+		this.checkCollectionsLoaded();
+	},
+
+	checkCollectionsLoaded:function(){
+		var imgs=bannersData.images;
+		for(var cnt=0,m=imgs.length;cnt<m;cnt++)
+			if(!imgs[cnt].loaded){
+				setTimeout($.proxy(this.checkCollectionsLoaded,this),100);
+				return;
+			}
 		this.page1.Activate();
 		//this.page3.Activate("googlead",['b240x400']);
 	},
+
+
 	_setOption: function( key, value ) {
         	this._super( key, value );
 	},
@@ -20,6 +42,28 @@ $.widget("snark.adedit",{
 	},
 
 /*custom*/
+
+	imageCollectionAdd:function(arr){
+		for(cnt=0,m=arr.length;cnt<m;cnt++)
+			arr[cnt]=this.imageAdd(arr[cnt]);
+	},
+	
+	imageAdd:function(src){
+		var img=new Image();
+		bannersData.images.push({
+			img:img,
+			loaded:false
+		});
+		var cnt=bannersData.images.length-1;
+		img.addEventListener('load',function(){
+			bannersData.images[cnt].loaded=true;
+			bannersData.images[cnt].width=this.width;
+			bannersData.images[cnt].height=this.height;
+		});
+		img.src=src;
+		return cnt;
+	},
+
 	globalVar:function(name,value){
 		var g=this.gdatavar;
 		if(!window[g]) window[g]={};
@@ -155,7 +199,9 @@ $.widget("snark.adedit",{
 			var _templateid=templateid;
 			var _banner={}
 			var _container=null;
-			var _lastClick='';
+			var _activeLayer=-1;
+			var _layers=[];
+			
 			_init(parentobj);
 
 			 function _init(parentobj){
@@ -190,13 +236,39 @@ $.widget("snark.adedit",{
 				_container.attr('data-id',templateid);
 				$(_container.find('#title')).html(tData.name);
 				$(_container.find('.workarea')).css({"width":tData.w,"height":tData.h});
-				$('<div></div>').addClass('logodrag').appendTo(_container.find('.workarea'));
+				$('<div></div>').addClass('bgcontainer').appendTo(_container.find('.workarea'));
+				$('<div></div>').addClass('logocontainer').appendTo(_container.find('.workarea'));
+				
 				_container.appendTo(parentobj);
 				$(_plugin.element.find('.templates .toolpanels')).clone().appendTo(_container.find('.tools'));
 
+				_container.find('[data-btn="newlogo"]').on('click',_newImgStart);
+				_container.find('[data-btn="newbg"]').on('click',_newImgStart);
+				_container.find('#newimg').on('change',_newImgChoose);
+				
 				_renderIcons()
 
 				_render()
+			}
+			
+			function _newImgStart(){
+				_container.find('#newimg')[0].form.reset();
+				$(_container.find('#newimg')).attr("data-type",$(this).attr('data-type')).click();
+			}
+
+			function _newImgChoose(){
+				if(this.files.length){
+					var type=$(this).attr('data-type');
+					var collection='custom';
+					var i=_plugin.imageAdd(
+							window.URL.createObjectURL(this.files[this.files.length-1])
+					);
+					bannersData.templates[_templateid][collection][type].push(i);
+					_banner[type].id=i;
+
+					_renderIcons();
+					_render();
+				}
 			}
 			
 			function _destroy(){
@@ -209,31 +281,45 @@ $.widget("snark.adedit",{
 				_fillImgs('sys','logo');
 			}
 			
+			function _updateLayers(tData){
+				_layers=[_layerBg(_banner.bg,1),_layerLogo(_banner.logo,2)];
+				for(var cnt=0,m=tData.texts.length;cnt<m;cnt++)
+					_layers.push(_layerText(tData.texts[cnt],cnt+3));
+			}
+			
 			function _render(){
+
+//mark selected icons
+				_container.find('.ico').attr('data-sel','');
+				if(_banner.bg.id)
+					_container.find('.ico[data-id="'+_banner.bg.id+'"]').attr('data-sel','1');
+				if(_banner.logo.id)
+					_container.find('.ico[data-id="'+_banner.logo.id+'"]').attr('data-sel','1');
+
+
+				var bgImg=(_banner.bg.id!=undefined)?bannersData.images[_banner.bg.id]:{loaded:true};
+				var logoImg=(_banner.logo.id!=undefined)?bannersData.images[_banner.logo.id]:{loaded:true};
+
+//if images aren't loaded - try to wait a bit
+				if(!bgImg.loaded || !logoImg.loaded)
+					return setTimeout($.proxy(_render,this),100);
+
 				var tData=bannersData.templates[_templateid];
 				var workarea=$(_container.find('.workarea'));
+				workarea.empty();
+				var layers=$(_container.find('.layers'));
+				layers.empty();
 
-//set bg
-				_container.find('.bg .ico').attr('data-sel','');
-				try {
-					var src=tData[_banner.bg.collection].bg[_banner.bg.id]
-					workarea.css('background-image','url('+src+')');
-					_container.find('.'+_banner.bg.collection+' .bg .ico[data-id="'+_banner.bg.id+'"]').attr('data-sel','1');
-				} catch(e){
-					workarea.css('background-image','none');
-				}				
+//buuld layers
+				_updateLayers(tData);
+				
+				for(var cnt=0,m=_layers.length;cnt<m;cnt++){
+					_layers[cnt].render(workarea,cnt==_activeLayer);
+					_layers[cnt].renderItem(layers,cnt==_activeLayer);
+				}
 
-				_container.find('.logo .ico').attr('data-sel','');
-				try {
-					var src=tData[_banner.logo.collection].logo[_banner.logo.id]
-					workarea.find('.logodrag img').remove();
-					$('<img>').attr('src',src).appendTo(workarea.find('.logodrag'));
-					_container.find('.'+_banner.logo.collection+' .logo .ico[data-id="'+_banner.logo.id+'"]').attr('data-sel','1');
-				} catch(e){
-					workarea.find('.logodrag img').remove();
-				}				
-
-				workarea.find('.logodrag').draggable({ disabled: (_lastClick!='logo') });
+				//workarea.find('.logodrag').draggable({ disabled: (_lastClick!='logo') });
+				//workarea.find('.logodrag').draggable({ disabled: false });
 
 			}
 
@@ -241,33 +327,113 @@ $.widget("snark.adedit",{
 				var obj=$(_container.find('.'+collection+' .'+type+' .icons'));
 				obj.find('a').off();
 				obj.find('a').remove();
+				var imgs=bannersData.images;
 				var arr=bannersData.templates[_templateid][collection][type];
 				for(var cnt=0,m=arr.length;cnt<m;cnt++)
 					$('<a></a>')
 						.attr({
-							"data-id":cnt,
-							"data-collection":collection,
+							"data-id":arr[cnt],
 							"data-type":type
 							})
 						.addClass('ico')
-						.css('backgroundImage','url('+arr[cnt]+')')
+						.css('backgroundImage','url('+imgs[arr[cnt]].img.src+')')
 						.appendTo(obj)
 						.on('click',_setImg);
 				return obj;			
 			}
 			
 			function _setImg(){
-				var type=$(this).attr('data-type');
-				var collection=$(this).attr('data-collection');
 				var id=$(this).attr('data-id');
-				if(type=='logo' && _banner[type].collection==collection && _banner[type].id==id){
-					collection='';
-					id=0;
+				var type=$(this).attr('data-type');
+				
+				if(_banner[type].id==id){
+//switch off
+					_banner[type]={};				
+				} else {
+					_banner[type].id=id;
 				}
-				_banner[type].collection=collection;
-				_banner[type].id=id;
-				_lastClick=type;
-				_render();
+				
+ 				_render();
+			}
+			
+			function _layerBg(data,cnt){
+				var _data=data;
+				var _cnt=cnt;
+				function _renderItem(container){
+					$('<div></div>').addClass('layer').html('Background').appendTo(container);
+				}
+				function _render(container){
+					if(_data.id && bannersData.images[data.id])
+						$(bannersData.images[data.id].img).clone().appendTo(
+							$('<div></div>')
+								.addClass('layer')
+								.css({
+									'z-index':_cnt,
+									'top':_data.top+'px',
+									'left':_data.left+'px'
+									})
+								.appendTo(container)
+						);
+				}
+				return {
+					renderItem:_renderItem,
+					render:_render
+				}
+			}
+
+			function _layerLogo(data,cnt){
+				var _data=data;
+				var _cnt=cnt;
+				function _renderItem(container){
+					$('<div></div>').addClass('layer').html('Logo').appendTo(container);
+				}
+				function _render(container){
+					if(_data.id && bannersData.images[data.id])
+						$(bannersData.images[data.id].img).clone().appendTo(
+							$('<div></div>')
+								.addClass('layer')
+								.css({
+									'z-index':_cnt,
+									'top':_data.top+'px',
+									'left':_data.left+'px'
+									})
+								.appendTo(container)
+						);
+					
+				}
+				return {
+					renderItem:_renderItem,
+					render:_render
+				}
+			}
+
+			function _layerText(data,cnt){
+				var _data=data;
+				var _cnt=cnt;
+				function _renderItem(container){
+					$('<div></div>').addClass('layer').html(_data.text).appendTo(container);
+				}
+				function _render(container){
+					console.log(_data);
+					if(_data.text)
+						$('<div></div>')
+							.addClass('layer')
+							.css({
+								'z-index':_cnt,
+								'color':_data.color,
+								'font-family':_data.font,
+								'font-size':_data.fontSize,
+								'text-align':_data.align,
+								'top':_data.top+'px',
+								'left':_data.left+'px'
+								})
+							.html(_data.text)
+							.appendTo(container)
+				}
+				return {
+					renderItem:_renderItem,
+					render:_render
+				}
 			}
 			
 			return {
